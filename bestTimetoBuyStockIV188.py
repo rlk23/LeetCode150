09c10d7663059f7143dd249dc03259c963cf5c8c
Rami L Kuttab <74058561+rlk23@users.noreply.github.com>
2025-01-09 13:46:12 -0600

    Update bestTimetoBuyStockIV188.py

--- bestTimetoBuyStockIV188.py
+++ bestTimetoBuyStockIV188.py
@@ -39,26 +39,23 @@ Constraints:
 
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
-        if not prices:
-            return 0
+        if not prices:  return 0
 
-        n = len(prices) 
+        n = len(prices)
 
-        if k >= n // 2:
-            return sum(max(prices[i] - prices[i-1],0) for i in range(1,n))
+        dp = [0] * n
 
-        dp = [[[0] * 2 for _ in range(k + 1)] for _ in range(n)]
+        if k > n:
+            B = [prices[i] - prices[i-1] for i in range(1,n)]
+            return sum([b for b in B if b > 0])
 
+        for t in range(k):
+            pos = -prices[0]
+            profit = 0
 
-
-        for t in range(k+1):
-            dp[0][t][1] = -prices[0]
-        for i in range(1,n):
-            for t in range(1, k+1):
-                dp[i][t][0] = max(dp[i-1][t][0] , dp[i-1][t][1] + prices[i])
-
-                dp[i][t][1] = max(dp[i-1][t][1] , dp[i-1][t-1][0] - prices[i])
-        
-        return dp[n-1][k][0]
-        
+            for i in range(1,n):
+                pos = max(pos,dp[i] - prices[i])
+                profit = max(profit, pos + prices[i])
+                dp[i] = profit
+        return dp[-1]
         
