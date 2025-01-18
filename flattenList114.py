48a415f76ec4c7d4f7c3cb4a0711e27e31807975
Rami L Kuttab <74058561+rlk23@users.noreply.github.com>
2025-01-17 19:11:08 -0600

    Create flattenList114.py

---
+++ flattenList114.py
@@ -0,0 +1,74 @@
+'''
+
+Given the root of a binary tree, flatten the tree into a "linked list":
+
+The "linked list" should use the same TreeNode class where the right child pointer points to the next node in the list and the left child pointer is always null.
+The "linked list" should be in the same order as a pre-order traversal of the binary tree.
+ 
+
+Example 1:
+
+
+Input: root = [1,2,5,3,4,null,6]
+Output: [1,null,2,null,3,null,4,null,5,null,6]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [0]
+Output: [0]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 2000].
+-100 <= Node.val <= 100
+ 
+
+Follow up: Can you flatten the tree in-place (with O(1) extra space)?
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+
+        if not root:
+            return None
+        if root.left is None and root.right is None:
+            return root
+        
+
+        if root.left:
+            self.flatten(root.left)
+
+            tempRight = root.right
+            root.right = root.left
+            root.left = None
+
+            current = root.right
+            while current.right:
+                current = current.right
+
+            # Attach the original right subtree
+            current.right = tempRight
+        
+        self.flatten(root.right)
+        
+
+
+        
+
+
+        
+
+
+
