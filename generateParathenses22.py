a94d22c0eedfd78398c0d42e9103c657f4768efd
Rami L Kuttab <74058561+rlk23@users.noreply.github.com>
2025-01-19 08:55:00 -0600

    Create generateParathenses22.py

---
+++ generateParathenses22.py
@@ -0,0 +1,43 @@
+'''
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+ 
+
+Example 1:
+
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+Example 2:
+
+Input: n = 1
+Output: ["()"]
+ 
+
+Constraints:
+
+1 <= n <= 8
+
+'''
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        stack = []
+        res = []
+
+        def backtrack(openN, closeN):
+            if openN == closeN == n:
+                res.append(''.join(stack))
+                return
+            
+            if openN < n:
+                stack.append("(")
+                backtrack(openN+1,closeN)
+                stack.pop()
+            if closeN < openN:
+                stack.append(")")
+                backtrack(openN, closeN+1)
+                stack.pop()
+            
+        backtrack(0,0)
+
+        return res
