1eda80f993ff701c2c93727a642b6e406fa22f48
Rami L Kuttab <74058561+rlk23@users.noreply.github.com>
2025-01-19 17:40:22 -0600

    Create leetcode152.py

---
+++ leetcode152.py
@@ -0,0 +1,43 @@
+'''
+Given an integer array nums, find a 
+subarray
+ that has the largest product, and return the product.
+
+The test cases are generated so that the answer will fit in a 32-bit integer.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,-2,4]
+Output: 6
+Explanation: [2,3] has the largest product 6.
+Example 2:
+
+Input: nums = [-2,0,-1]
+Output: 0
+Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+ 
+
+Constraints:
+
+1 <= nums.length <= 2 * 104
+-10 <= nums[i] <= 10
+The product of any subarray of nums is guaranteed to fit in a 32-bit integer.
+
+'''
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        maxProduct = nums[0]
+        minProduct = nums[0]
+        result = nums[0]
+        n = len(nums)
+
+        for i in range(1,n):
+            temp = maxProduct
+            maxProduct = max(nums[i], nums[i] * maxProduct, nums[i] * minProduct)
+            minProduct = min(nums[i], nums[i] * temp , nums[i] * minProduct) 
+            result = max(maxProduct, result)
+        
+        return result 
