97711deb8fe43778de9bf08ae416f20c9a4229fe
Rami L Kuttab <74058561+rlk23@users.noreply.github.com>
2025-01-23 11:43:09 -0600

    Update leetcode116.py

--- leetcode116.py
+++ leetcode116.py
@@ -47,27 +47,33 @@ class Node:
         self.next = next
 """
 
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
         if not root:
             return None
         
-        queue = collections.deque([root])
+        current_level = root
 
-        while queue:
-            level = []
+        while current_level and current_level.left:
+            current_node = current_level
+            while current_node:
+                current_node.left.next = current_node.right
 
-            for _ in range(len(queue)):
-                node = queue.popleft()
-                level.append(node)
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-                
-            for i in range(len(level)):
-                if i == len(level)-1:
-                    level[i].next = None
-                else:
-                    level[i].next = level[i+1]
+                if current_node.next:
+                    current_node.right.next = current_node.next.left
+
+                current_node = current_node.next
+            
+            current_level = current_level.left
         return root
+        
