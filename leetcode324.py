810d381a964e35ee8884801ad434051d4a986c0b
Rami L Kuttab <74058561+rlk23@users.noreply.github.com>
2025-03-11 06:50:21 -0500

    Create leetcode324.py

---
+++ leetcode324.py
@@ -0,0 +1,52 @@
+'''
+Given an integer array nums, reorder it such that nums[0] < nums[1] > nums[2] < nums[3]....
+
+You may assume the input array always has a valid answer.
+
+ 
+
+Example 1:
+
+Input: nums = [1,5,1,1,6,4]
+Output: [1,6,1,5,1,4]
+Explanation: [1,4,1,5,1,6] is also accepted.
+Example 2:
+
+Input: nums = [1,3,2,2,3,1]
+Output: [2,3,1,3,1,2]
+ 
+
+Constraints:
+
+1 <= nums.length <= 5 * 104
+0 <= nums[i] <= 5000
+It is guaranteed that there will be an answer for the given input nums.
+ 
+
+Follow Up: Can you do it in O(n) time and/or in-place with O(1) extra space?
+'''
+
+
+class Solution:
+    def wiggleSort(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        nums.sort()
+
+        n = len(nums)
+
+        sorted_nums = nums[:]
+
+        left, right = (n + 1) // 2 - 1, n - 1
+
+        for i in range(n):
+            if i % 2 == 0:
+                nums[i] = sorted_nums[left]
+                left -= 1
+            else:
+                nums[i] = sorted_nums[right]
+                right -= 1
+        
+
+
