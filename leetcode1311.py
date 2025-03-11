d69699d34531b13ff4f7971b5e9b3a060f1da53e
Rami L Kuttab <74058561+rlk23@users.noreply.github.com>
2025-03-11 16:53:04 -0500

    Create leetcode1311.py

---
+++ leetcode1311.py
@@ -0,0 +1,70 @@
+'''
+There are n people, each person has a unique id between 0 and n-1. Given the arrays watchedVideos and friends, where watchedVideos[i] and friends[i] contain the list of watched videos and the list of friends respectively for the person with id = i.
+
+Level 1 of videos are all watched videos by your friends, level 2 of videos are all watched videos by the friends of your friends and so on. In general, the level k of videos are all watched videos by people with the shortest path exactly equal to k with you. Given your id and the level of videos, return the list of videos ordered by their frequencies (increasing). For videos with the same frequency order them alphabetically from least to greatest. 
+
+ 
+
+Example 1:
+
+
+
+Input: watchedVideos = [["A","B"],["C"],["B","C"],["D"]], friends = [[1,2],[0,3],[0,3],[1,2]], id = 0, level = 1
+Output: ["B","C"] 
+Explanation: 
+You have id = 0 (green color in the figure) and your friends are (yellow color in the figure):
+Person with id = 1 -> watchedVideos = ["C"] 
+Person with id = 2 -> watchedVideos = ["B","C"] 
+The frequencies of watchedVideos by your friends are: 
+B -> 1 
+C -> 2
+Example 2:
+
+
+
+Input: watchedVideos = [["A","B"],["C"],["B","C"],["D"]], friends = [[1,2],[0,3],[0,3],[1,2]], id = 0, level = 2
+Output: ["D"]
+Explanation: 
+You have id = 0 (green color in the figure) and the only friend of your friends is the person with id = 3 (yellow color in the figure).
+ 
+
+Constraints:
+
+n == watchedVideos.length == friends.length
+2 <= n <= 100
+1 <= watchedVideos[i].length <= 100
+1 <= watchedVideos[i][j].length <= 8
+0 <= friends[i].length < n
+0 <= friends[i][j] < n
+0 <= id < n
+1 <= level < n
+if friends[i] contains j, then friends[j] contains i
+
+'''
+class Solution:
+    def watchedVideosByFriends(self, watchedVideos: List[List[str]], friends: List[List[int]], id: int, level: int) -> List[str]:
+        
+
+        queue = deque([id])
+        visited = set([id])
+
+        current_level = 0
+
+        while queue and current_level < level:
+            size = len(queue)
+            for _ in range(size):
+                person = queue.popleft()
+                for friendd in friends[person]:
+                    if friend not in visited:
+                        visited.add(friend)
+                        queue.append(friend)
+            current_level += 1
+
+        video_count = Counter()
+
+        while queue:
+            person = queue.popleft()
+            for video in watchedVideos[person]:
+                video_count[video] += 1
+
+        return sorted(video_count.keys(), key = lambda x:(video_count[x],x))
